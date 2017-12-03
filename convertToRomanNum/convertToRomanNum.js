49b6630b2b815bf61901022a26d3af5175157521
raphsutti <neoster9@gmail.com>
2017-12-03 14:20:31 +1100

    Convert numerical numbers to roman numeral style upto 1000

---
+++ convertToRomanNum/convertToRomanNum.js
@@ -0,0 +1,16 @@
+function convertToRoman(num) { 
+  var rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]; 
+  var dec = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]; 
+
+  var result = ""; 
+  for (var i=0; i<dec.length; i++) { 
+    while (dec[i] <= num) { 
+      result += rom[i]; 
+      num -= dec[i]; 
+    } 
+  } 
+
+  return result 
+} 
+
+convertToRoman(506); 
