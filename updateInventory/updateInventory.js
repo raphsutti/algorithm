92f807fd231c8eff7cda4edbbd3490a044b30d55
raphsutti <neoster9@gmail.com>
2017-12-11 19:45:48 +1100

    added comments

--- updateInventory/updateInventory.js
+++ updateInventory/updateInventory.js
@@ -1,20 +1,22 @@
 function updateInventory(arr1, arr2) {
-  // All inventory must be accounted for or you're fired!
   for (var i=0; i<arr2.length; i++) {
+    // look through every item in arr1
     var invenExist = arr1.every(function(element) {
       return arr2[i][1] != element[1];
     });
-    // if doesnt exist, add to arr1, else add it's value
-    if(invenExist) {
-      arr1.push(arr2[i])
+    // if doesn't exist, add name and value to arr1, else total up the value
+    if (invenExist) {
+      arr1.push(arr2[i]);
     } else {
+      // find index of arr1
       var indexVal = arr1.findIndex(function(element) {
         return element[1] === arr2[i][1];
       });
+      // add the values of that arr1[index]
       arr1[indexVal][0] += arr2[i][0];
     }
   }
-  // sort by second item
+  // sort by item name - index 1
   return arr1.sort(function(a,b) {
     return a[1] < b[1] ? -1 : 1
   });
