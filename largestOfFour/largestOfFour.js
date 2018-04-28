d753c63b9b2c28d85c6e72797916368baf9a8d55
Raphael Suttiyotin <neoster9@gmail.com>
2018-04-28 18:38:03 +1000

    Update largestOfFour.js
    refactored and comments

--- largestOfFour/largestOfFour.js
+++ largestOfFour/largestOfFour.js
@@ -1,9 +1,11 @@
+// function to return array with largest numbers from the sub arrays
 function largestOfFour(arr) {
-  // You can do this! 
-  var maxArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    maxArr.push(Math.max.apply(Math, arr[i]));
-  }
-  return maxArr;
+  var result =[];
+  // loop through each elem
+  arr.forEach(function(elem){
+    // use max and apply to reduce result into one then push into result array
+    result.push(Math.max.apply(null,elem));
+  });
+  return result;
 }
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
