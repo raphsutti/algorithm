065ccce65c2bb7aea38f331a341f0424458e56f9
Raphael Suttiyotin <neoster9@gmail.com>
2018-04-28 17:34:42 +1000

    Update titleCase.js
    refactored

--- titleCase/titleCase.js
+++ titleCase/titleCase.js
@@ -1,16 +1,15 @@
 function titleCase(str) {
-  var strLow = str.toLowerCase(); 
-  var strArr = strLow.split(" ");
-  var result = "";
-  var final = "";
-  for (var i=0; i<strArr.length; i++) {
-    var charArr = strArr[i].split("");
-    charArr[0] = charArr[0].toUpperCase();
-    result = charArr.join("");
-    final += " ";
-    final += result;
-  }
-  return final.replace(/^\s+|\s+$/g, "");
+  var resultArray = []
+  str.split(" ").forEach(function(elem){
+    var word = ""
+    word += elem[0].toUpperCase()
+    for(var i=1; i<elem.length;i++) {
+      word +=elem[i].toLowerCase()
+    }
+    resultArray.push(word)
+  })
+  return resultArray.join(" ")
 }
 
 titleCase("I'm a little tea pot");
+
