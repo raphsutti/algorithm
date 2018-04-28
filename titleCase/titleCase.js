d7f7e27266fda02d3731ef10bd0e39b907522881
Raphael Suttiyotin <neoster9@gmail.com>
2018-04-28 17:46:08 +1000

    Update titleCase.js
    comment and cleanup

--- titleCase/titleCase.js
+++ titleCase/titleCase.js
@@ -1,14 +1,21 @@
+// function to return every word capitalised 
 function titleCase(str) {
-  var resultArray = []
+  // result to be stored in this array
+  var resultArray = [];
+  // split input string into array
   str.split(" ").forEach(function(elem){
-    var word = ""
-    word += elem[0].toUpperCase()
+    // current elem word result
+    var word = "";
+    // add first letter as capitalised
+    word += elem[0].toUpperCase();
+    // loop through and add the rest of the letters, skip first letter
     for(var i=1; i<elem.length;i++) {
-      word +=elem[i].toLowerCase()
+      // make sure other letters are lower case
+      word +=elem[i].toLowerCase();
     }
-    resultArray.push(word)
-  })
-  return resultArray.join(" ")
+    resultArray.push(word);
+  });
+  return resultArray.join(" ");
 }
 
 titleCase("I'm a little tea pot");
