aa5434bc8384bcab124c5f7722188d11e58729f2
Raphael Suttiyotin <neoster9@gmail.com>
2018-04-29 20:43:10 +1000

    Update repeatStrNumTimes.js
    comments

--- repeatStrNumTimes.js
+++ repeatStrNumTimes.js
@@ -1,14 +1,17 @@
-
+// repeat str, num times - if num < 1, return ""
 function repeatStringNumTimes(str, num) {
   // repeat after me
-  if (num <0) {
+  var newStr = "";
+  // catch num less than 1
+  if (num < 1) {
     return "";
-  }
-  var newStr ="";
-  for (var i=0; i<num; i++) {
-    newStr += str;
+  } else {
+    // add to str num times
+    for(var i=0; i<num; i++) {
+      newStr += str;
+    }
   }
   return newStr;
 }
 
-repeatStringNumTimes("abc", 2);
+repeatStringNumTimes("abc", 3);
