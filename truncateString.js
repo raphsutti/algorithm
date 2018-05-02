8edfcfc371317cdadba9b9636d3f87ee8cc43353
Raphael Suttiyotin <neoster9@gmail.com>
2018-05-02 19:07:04 +1000

    Refactor

--- truncateString.js
+++ truncateString.js
@@ -1,14 +1,15 @@
-
+// Truncate str by max characters num
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  if (str.length < 3 || num <3) {
-    var slice = str.slice(str,num);
-    return slice += "...";
-  } else if (str.length>num) {
-    var slice = str.slice(str, num-3);
-    return slice += "...";
+  var strArray = str.split("");
+  // str length longer than num
+  if (strArray.length > num && num > 3) {
+    return strArray.splice(0,num-3).join("") + "...";
+  // num <=3
+  } else if (num <=3) {
+    return strArray.splice(0,num).join("") + "...";
+  } else {
+    return str;
   }
-  return str;
 }
 
-truncateString("Absolutely Longer", 2);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
