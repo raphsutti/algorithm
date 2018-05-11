1b5e4ede6c7bb5189cd2e4ec6c304ff581c89fa1
Raphael Suttiyotin <neoster9@gmail.com>
2018-05-11 13:10:37 +1000

    Use fat arrow

--- bouncer.js
+++ bouncer.js
@@ -1,9 +1,7 @@
 // Filter out falsy values
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
-  return arr.filter(function(x) {
-    return Boolean(x);
-  });
+  return arr.filter(x => Boolean(x));
 }
 
 bouncer([7, "ate", "", false, 9]);
