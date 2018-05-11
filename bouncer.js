cf31e179826b2abc0cd5a8db7891f12514d0e1f9
Raphael Suttiyotin <neoster9@gmail.com>
2018-05-11 13:11:18 +1000

    another way

--- bouncer.js
+++ bouncer.js
@@ -5,3 +5,12 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+// Even cleaner but less clear
+// Filter out falsy values
+// function bouncer(arr) {
+//   // Don't show a false ID to this bouncer.
+//   return arr.filter(Boolean);
+// }
+
+// bouncer([7, "ate", "", false, 9]);
