9248843f882c5a0929e84f47616b888f70c19226
Raphael Suttiyotin <neoster9@gmail.com>
2018-05-12 10:33:03 +1000

    added alternative solution

--- destroyer.js
+++ destroyer.js
@@ -7,4 +7,12 @@ function destroyer(arr) {
   return arr
 }
 
+// using includes
+// function destroyer(arr) {
+//   var args = Array.from(arguments).slice(1);
+//   return arr.filter(function(val) {
+//     return !args.includes(val);
+//   });
+// }
+
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
