b4439a05482321bd8c679fe8b4b111f67da9a4f6
Raph Sutti <neoster9@gmail.com>
2019-05-05 09:12:29 +1000

    initial commit

--- findSmallestArray.py
+++ findSmallestArray.py
@@ -3,16 +3,19 @@
 # If all elements in array < 0, return 1
 def solution(A):
     # write your code in Python 3.6
-    maxNum = max(A)
     result = []
-    for i in range(1,maxNum):
+    for i in range(1,max(A)):
         if i not in A:
             result.append(i)
     
+    # If all highest value is a negative, return 1
     if max(A) < 0:
         return 1
+    # If no missing values found, return highest value + 1
     elif not result:
         return max(A) + 1
+    # Return smallest number 
     else:
         return min(result)
-solution([1,3,6,4,1,2])
+
+print(solution([1,3,6,4,1,2])) # returns 5
