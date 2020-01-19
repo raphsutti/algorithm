003b8fdd2c333bb27d4a461376b7316eaefc699b
Raph Sutti <neoster9@gmail.com>
2020-01-19 19:09:10 +1100

    refactor

--- countVowels.py
+++ countVowels.py
@@ -1,9 +1,9 @@
 def getCount(inputStr):
-    num_vowels = 0 
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    for letter in inputStr:
-        if letter in vowels:
-            num_vowels += 1
-    return num_vowels
-
+#    num_vowels = 0 
+#    vowels = ['a', 'e', 'i', 'o', 'u']
+#    for letter in inputStr:
+#        if letter in vowels:
+#            num_vowels += 1
+#    return num_vowels
+	return sum(char in 'aeiou' for char in inputStr)
 # getCount("abracadabra") #5
