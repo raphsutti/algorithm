30841de04bf0504f3786a8fa780961849cb3c939
Raph Sutti <neoster9@gmail.com>
2020-01-22 19:38:23 +1100

    initial commit

---
+++ sumArrays.py
@@ -0,0 +1,10 @@
+def array_plus_array(arr1,arr2):
+    return sum(arr1) + sum(arr2)
+    
+'''
+Test.assert_equals(array_plus_array([1, 2, 3], [4, 5, 6]), 21)
+Test.assert_equals(array_plus_array([-1, -2, -3], [-4, -5, -6]), -21)
+Test.assert_equals(array_plus_array([0, 0, 0], [4, 5, 6]), 15)
+Test.assert_equals(array_plus_array([100, 200, 300], [400, 500, 600]), 2100)
+'''
+
