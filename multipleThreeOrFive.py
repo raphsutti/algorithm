39040de9e520e0defc34ad1c16317c8c0a76d45a
Raph Sutti <neoster9@gmail.com>
2020-01-27 12:15:31 +1100

    refactor

--- multipleThreeOrFive.py
+++ multipleThreeOrFive.py
@@ -1,8 +1,4 @@
 def solution(number):
-    result = 0
-    for item in range(1,number):
-        if item % 3 == 0 or item % 5 == 0:
-            result += item
-    return result
+    return sum(item for item in range(number) if item % 3 == 0 or item % 5 == 0)
     
 # test.assert_equals(solution(10), 23)
