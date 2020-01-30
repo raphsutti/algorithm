a43853d7a18531d237efc92de844e94c97355f31
Raph Sutti <neoster9@gmail.com>
2020-01-31 09:01:36 +1100

    refactored

--- highestLowest.py
+++ highestLowest.py
@@ -1,8 +1,4 @@
 def high_and_low(numbers):
-    array = numbers.split(" ")
-    numbersArray = []
-    for item in range(0, len(array)):
-        numbersArray.append(int(array[item]))
-    return(str(max(numbersArray)) + " " + str(min(numbersArray)))
-    
+   result = [int(string) for string in numbers.split(" ")]
+   return str(max(result)) + " " + str(min(result)) 
 #     Test.assert_equals(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
